e5243fdea9c66773530365dc3f725cf48a52724d
ichbindemiurge <32437438+ichbindemiurge@users.noreply.github.com>
2021-04-22 14:21:20 +0300

    Revert "changed MappingDTOtoClass"

--- src/main/java/com/example/demo/mapping/MappingDTOtoClass.java
+++ src/main/java/com/example/demo/mapping/MappingDTOtoClass.java
@@ -4,26 +4,50 @@ import com.example.demo.dto.AuthorDTO;
 import com.example.demo.dto.BookDTO;
 import com.example.demo.model.Author;
 import com.example.demo.model.Book;
+import com.example.demo.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MappingDTOtoClass {
 
-    public BookDTO BookToDTO(Book book) {
+    private AuthorService authorService;
+
+    public BookDTO mapToBookDTO (Book book) {
         BookDTO bookDTO = new BookDTO();
         bookDTO.setId(book.getId());
         bookDTO.setBookName(book.getBookName());
         bookDTO.setBookDescription(book.getBookDescription());
+        //bookDTO.setAuthorsIDsDTOList(book.getAuthorsList());
+
+        List<Author> authorsList = book.getAuthorsList();
+        List<String> authorIDs = new ArrayList<>(authorsList.size());
+        for(Author author : authorsList){
+            authorIDs.add(String.valueOf(author.getId()));
+        }
+        bookDTO.setAuthorsIDsDTOList(authorIDs);
         return bookDTO;
     }
 
-    public Book DTOToBook(BookDTO bookDTO) {
+    public Book mapToDTOBook (BookDTO bookDTO) {
         Book book = new Book();
         book.setBookName(bookDTO.getBookName());
         book.setBookDescription(bookDTO.getBookDescription());
+
+        List<String> authorIDs = bookDTO.getAuthorsIDsDTOList();
+        List<Author> authorsList = new ArrayList<>(authorIDs.size());
+        for(String authorID : authorIDs) {
+            authorsList.add(authorService.getAuthorById(authorID));
+        }
+        book.setAuthorsList(authorsList);
+
         return book;
     }
 
-    public AuthorDTO DTOToAuthor(Author author) {
+    public AuthorDTO mapToAuthor (Author author) {
         AuthorDTO authorDTO = new AuthorDTO();
         authorDTO.setId(author.getId());
         authorDTO.setAuthorName(author.getAuthorName());
@@ -31,7 +55,7 @@ public class MappingDTOtoClass {
         return authorDTO;
     }
 
-    public Author authorToDTO(AuthorDTO authorDTO){
+    public Author mapAuthorToDTO (AuthorDTO authorDTO){
         Author author = new Author();
         author.setId(authorDTO.getId());
         author.setAuthorName(authorDTO.getAuthorName());
